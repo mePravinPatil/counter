5de7bed039f7c94b8f41b552a65119711d3edcc7
Pravin Patil <87909133+mePravinPatil@users.noreply.github.com>
2021-09-18 09:58:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+var counter=10;
+
+
+ setTimeout(() => {
+    document.write(10);
+    setTimeout(() => {
+    document.body.innerHTML = "";
+    
+    setTimeout(() => {
+        document.write(9);
+        setTimeout(() => {
+          document.body.innerHTML = "";
+          
+        
+        
+        setTimeout(() => {
+            document.write(8);
+            setTimeout(() => {
+              document.body.innerHTML = "";
+              
+          setTimeout(() => {
+            document.write(7);
+            setTimeout(() => {
+              document.body.innerHTML = "";
+              
+            setTimeout(() => {
+                document.write(6);
+                setTimeout(() => {
+                  document.body.innerHTML = "";
+                  
+              setTimeout(() => {
+                document.write(5);
+                setTimeout(() => {
+                  document.body.innerHTML = "";
+                  
+                setTimeout(() => {
+                    document.write(4);
+                    setTimeout(() => {
+                      document.body.innerHTML = "";
+                      
+                    setTimeout(() => {
+                        document.write(3);
+                        setTimeout(() => {
+                          document.body.innerHTML = "";
+                          
+                       setTimeout(() => {
+                        document.write(2);
+                        setTimeout(() => {
+                          document.body.innerHTML = "";
+                          
+                         setTimeout(() => {
+                            document.write(1);
+                            setTimeout(() => {
+                              document.body.innerHTML = "";
+                              
+                            setTimeout(() => {
+                                document.write("Happy New Year");
+                                     },500);
+                                  },500);
+                               },500);
+                            },500);
+                          },500);
+                        },1000);
+                      },1000);
+                    },1000);
+                  },1000);
+                },1000);
+              },1000);
+            },1000);
+          },1000);
+        },1000);
+      },1000);
+    },1000);
+  },1000);
+},1000);
+},1000);
+},1000);
+},1000);
